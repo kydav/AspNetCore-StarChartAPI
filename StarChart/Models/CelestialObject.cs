8634a2cf7d4652b4643e7f885c98ff3bde606975
Kyler Davis <kyler.davis@maritzcx.com>
2020-02-08 17:04:05 -0700

    3rd module

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int id;
+        [Required]
+        public string name;
+        public int? OrbitedObjectId;
+        [NotMapped]
+        public List<CelestialObject> Satellites;
+        public TimeSpan OrbitalPeriod;
+
+        public CelestialObject()
+        {
+            
+        }
+    }
+}
